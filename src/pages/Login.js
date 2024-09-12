1885ce4ed37b55e48de38f0e0e889a308ade8bc7
MOHIT SARSAHAY <142138405+mohit07dec@users.noreply.github.com>
2024-09-12 11:44:58 +0530

    fix: auth ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import { handleError, handleSuccess } from '../utils';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text, 
+  Link,
+  Container,
+} from '@chakra-ui/react';
 
 function Login() {
-
     const [loginInfo, setLoginInfo] = useState({
         email: '',
         password: ''
@@ -14,17 +25,14 @@ function Login() {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        console.log(name, value);
-        const copyLoginInfo = { ...loginInfo };
-        copyLoginInfo[name] = value;
-        setLoginInfo(copyLoginInfo);
+        setLoginInfo({ ...loginInfo, [name]: value });
     }
 
     const handleLogin = async (e) => {
         e.preventDefault();
         const { email, password } = loginInfo;
         if (!email || !password) {
-            return handleError('email and password are required')
+            return handleError('Email and password are required')
         }
         try {
             const url = `http://localhost:8080/auth/login`;
@@ -50,43 +58,51 @@ function Login() {
             } else if (!success) {
                 handleError(message);
             }
-            console.log(result);
         } catch (err) {
             handleError(err);
         }
     }
 
     return (
-        <div className='container'>
-            <h1>Login</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        onChange={handleChange}
-                        type='email'
-                        name='email'
-                        placeholder='Enter your email...'
-                        value={loginInfo.email}
-                    />
-                </div>
-                <div>
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        onChange={handleChange}
-                        type='password'
-                        name='password'
-                        placeholder='Enter your password...'
-                        value={loginInfo.password}
-                    />
-                </div>
-                <button type='submit'>Login</button>
-                <span>Does't have an account ?
-                    <Link to="/signup">Signup</Link>
-                </span>
-            </form>
+        <Container maxW="container.sm" centerContent>
+            <Box padding="4" maxWidth="400px" width="100%">
+                <VStack spacing={8} align="stretch">
+                    <Heading as="h1" size="xl" textAlign="center">Login</Heading>
+                    <form onSubmit={handleLogin}>
+                        <VStack spacing={4}>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor='email'>Email</FormLabel>
+                                <Input
+                                    onChange={handleChange}
+                                    type='email'
+                                    name='email'
+                                    placeholder='Enter your email...'
+                                    value={loginInfo.email}
+                                />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor='password'>Password</FormLabel>
+                                <Input
+                                    onChange={handleChange}
+                                    type='password'
+                                    name='password'
+                                    placeholder='Enter your password...'
+                                    value={loginInfo.password}
+                                />
+                            </FormControl>
+                            <Button type='submit' colorScheme="blue" width="full">Login</Button>
+                        </VStack>
+                    </form>
+                    <Text textAlign="center">
+                        Don't have an account?{' '}
+                        <Link as={RouterLink} to="/signup" color="blue.500">
+                            Sign up
+                        </Link>
+                    </Text>
+                </VStack>
+            </Box>
             <ToastContainer />
-        </div>
+        </Container>
     )
 }
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import { handleError, handleSuccess } from '../utils';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  Link,
+  Container,
+} from '@chakra-ui/react';
 
 function Signup() {
-
     const [signupInfo, setSignupInfo] = useState({
         name: '',
         email: '',
@@ -12,19 +23,17 @@ function Signup() {
     })
 
     const navigate = useNavigate();
+
     const handleChange = (e) => {
         const { name, value } = e.target;
-        console.log(name, value);
-        const copySignupInfo = { ...signupInfo };
-        copySignupInfo[name] = value;
-        setSignupInfo(copySignupInfo);
+        setSignupInfo({ ...signupInfo, [name]: value });
     }
 
     const handleSignup = async (e) => {
         e.preventDefault();
         const { name, email, password } = signupInfo;
         if (!name || !email || !password) {
-            return handleError('name, email and password are required')
+            return handleError('Name, email and password are required')
         }
         try {
             const url = `http://localhost:8080/auth/signup`;
@@ -48,53 +57,62 @@ function Signup() {
             } else if (!success) {
                 handleError(message);
             }
-            console.log(result);
         } catch (err) {
             handleError(err);
         }
     }
+
     return (
-        <div className='container'>
-            <h1>Signup</h1>
-            <form onSubmit={handleSignup}>
-                <div>
-                    <label htmlFor='name'>Name</label>
-                    <input
-                        onChange={handleChange}
-                        type='text'
-                        name='name'
-                        autoFocus
-                        placeholder='Enter your name...'
-                        value={signupInfo.name}
-                    />
-                </div>
-                <div>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        onChange={handleChange}
-                        type='email'
-                        name='email'
-                        placeholder='Enter your email...'
-                        value={signupInfo.email}
-                    />
-                </div>
-                <div>
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        onChange={handleChange}
-                        type='password'
-                        name='password'
-                        placeholder='Enter your password...'
-                        value={signupInfo.password}
-                    />
-                </div>
-                <button type='submit'>Signup</button>
-                <span>Already have an account ?
-                    <Link to="/login">Login</Link>
-                </span>
-            </form>
+        <Container maxW="container.sm" centerContent>
+            <Box padding="4" maxWidth="400px" width="100%">
+                <VStack spacing={8} align="stretch">
+                    <Heading as="h1" size="xl" textAlign="center">Sign Up</Heading>
+                    <form onSubmit={handleSignup}>
+                        <VStack spacing={4}>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor='name'>Name</FormLabel>
+                                <Input
+                                    onChange={handleChange}
+                                    type='text'
+                                    name='name'
+                                    autoFocus
+                                    placeholder='Enter your name...'
+                                    value={signupInfo.name}
+                                />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor='email'>Email</FormLabel>
+                                <Input
+                                    onChange={handleChange}
+                                    type='email'
+                                    name='email'
+                                    placeholder='Enter your email...'
+                                    value={signupInfo.email}
+                                />
+                            </FormControl>
+                            <FormControl isRequired>
+                                <FormLabel htmlFor='password'>Password</FormLabel>
+                                <Input
+                                    onChange={handleChange}
+                                    type='password'
+                                    name='password'
+                                    placeholder='Enter your password...'
+                                    value={signupInfo.password}
+                                />
+                            </FormControl>
+                            <Button type='submit' colorScheme="blue" width="full">Sign Up</Button>
+                        </VStack>
+                    </form>
+                    <Text textAlign="center">
+                        Already have an account?{' '}
+                        <Link as={RouterLink} to="/login" color="blue.500">
+                            Log in
+                        </Link>
+                    </Text>
+                </VStack>
+            </Box>
             <ToastContainer />
-        </div>
+        </Container>
     )
 }
 
